92b5832a5770138decc5a2dbecad8cd0ceee7a06
thulerjoao <thuler_lima@hotmail.com>
2022-03-18 22:13:52 -0300

    commit v1.1

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -27,20 +27,31 @@ const elementoImg = document.querySelector("#imagem01");
 elementoBtn.addEventListener("click", ()=>{
     if(elementoBtn.value == "primeiro"){
         elementoBtn.innerText="Next"
-        frase.innerText = `"As ameaças doem, mas não tanto quanto uma faca serrilhada"`;
+        frase.innerText = `"Algumas das melhores histórias de amor começam com um homicídio."`;
         elementoImg.src = "./assets/IMG/imagem02.png";
         elementoImg.style.cssText = 'width: 46%;'+'margin-top: 3rem;';
         elementoBtn.value = "segundo";    
     }else if(elementoBtn.value == "segundo"){
-        frase.innerText = `"Terceira frase aqui"`;
+        frase.innerText = `"As ameaças doem, mas não tanto quanto uma faca serrilhada"`;
         elementoImg.src = "./assets/IMG/imagem03.png";
         elementoImg.style.cssText = 'width: 46%;'+'margin-top: 10rem;'+'margin-bottom:4rem;';
         elementoBtn.value = "terceiro";
-    }else if(elementoBtn.value = "terceiro"){
-        frase.innerText = `"Quarta frase aqui"`;
+    }else if(elementoBtn.value == "terceiro"){
+        frase.innerText = `"Água oxigenada e limão para remover manchas de sangue."`;
         elementoImg.src = "./assets/IMG/imagem04.png";
         elementoImg.style.cssText = 'width: 50%;'+'margin-top: 6rem;';
+        elementoBtn.value = "quarto";
+    }
+    else if(elementoBtn.value == "quarto"){
+        frase.innerText = `"Contras de ser um super-herói: todos são idiotas treinados para respeitar a lei."`;
+        elementoImg.src = "./assets/IMG/imagem06.png";
+        elementoImg.style.cssText = 'width: 46%;';
+        elementoBtn.value = "";
+    }else{
+        elementoBtn.innerText="Recomeçar"
+        frase.innerText = `Por hoje é só... Mas clica aí para ver de novo, vai!`;
+        elementoImg.src = "./assets/IMG/imagem05.png";
+        elementoImg.style.cssText = 'width: 84%;'+'margin-top: 7rem;';
         elementoBtn.value = "primeiro";
     }
 });
- 
