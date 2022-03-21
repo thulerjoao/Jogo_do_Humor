b6db4b7c1fa5f19d242d7026cbc341eb1273a9a8
thulerjoao <thuler_lima@hotmail.com>
2022-03-20 23:45:01 -0300

    commit final

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,7 @@ const itens = document.querySelectorAll("[data-anime]");
 let frase = document.querySelector("[data-frase]");
 let elementoBtn = document.querySelector("#alterar", "[data-btn]");
 const elementoImg = document.querySelector("#imagem01");
+let som = new Audio();
 
 //contrle das animações pelo js:
 
@@ -29,7 +30,7 @@ window.addEventListener("scroll", () =>{
 
 elementoBtn.addEventListener("click", ()=>{
     if(elementoBtn.value == "primeiro"){
-        let som = new Audio("./assets/SONGS/audio02.mp3")
+        som = new Audio("./assets/SONGS/audio02.mp3")
         som.play();
         elementoBtn.innerText="Next"
         frase.innerText = `"Algumas das melhores histórias de amor começam com um homicídio."`;
@@ -37,14 +38,14 @@ elementoBtn.addEventListener("click", ()=>{
         elementoImg.style.cssText = 'width: 46%;'+'margin-top: 3rem;';
         elementoBtn.value = "segundo";    
     }else if(elementoBtn.value == "segundo"){
-        let som = new Audio("./assets/SONGS/audio01.mp3")
+        som = new Audio("./assets/SONGS/audio01.mp3")
         som.play();
         frase.innerText = `"As ameaças doem, mas não tanto quanto uma faca serrilhada"`;
         elementoImg.src = "./assets/IMG/imagem03.png";
         elementoImg.style.cssText = 'width: 46%;'+'margin-top: 10rem;'+'margin-bottom:4rem;';
         elementoBtn.value = "terceiro";
     }else if(elementoBtn.value == "terceiro"){
-        let som = new Audio("./assets/SONGS/audio02.mp3")
+        som = new Audio("./assets/SONGS/audio02.mp3")
         som.play();
         frase.innerText = `"Água oxigenada e limão para remover manchas de sangue."`;
         elementoImg.src = "./assets/IMG/imagem04.png";
@@ -52,14 +53,14 @@ elementoBtn.addEventListener("click", ()=>{
         elementoBtn.value = "quarto";
     }
     else if(elementoBtn.value == "quarto"){
-        let som = new Audio("./assets/SONGS/audio03.mp3")
+        som = new Audio("./assets/SONGS/audio03.mp3")
         som.play();
         frase.innerText = `"Contras de ser um super-herói: todos são idiotas treinados para respeitar a lei."`;
         elementoImg.src = "./assets/IMG/imagem06.png";
         elementoImg.style.cssText = 'width: 46%;';
         elementoBtn.value = "";
     }else{
-        let som = new Audio("./assets/SONGS/audio04.mp3")
+        som = new Audio("./assets/SONGS/audio04.mp3")
         som.play();
         elementoBtn.innerText="Recomeçar"
         frase.innerText = `Por hoje é só... Mas clica aí para ver de novo, vai!`;
