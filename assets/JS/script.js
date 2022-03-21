2253b3c4c6830b6b40f26574c28a70ac34d6085f
thulerjoao <thuler_lima@hotmail.com>
2022-03-20 23:41:58 -0300

    commit final

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,7 @@
 const itens = document.querySelectorAll("[data-anime]");
 let frase = document.querySelector("[data-frase]");
 let elementoBtn = document.querySelector("#alterar", "[data-btn]");
-const elementoImg = document.querySelector("#imagem01"); 
+const elementoImg = document.querySelector("#imagem01");
 
 //contrle das animações pelo js:
 
@@ -29,28 +29,38 @@ window.addEventListener("scroll", () =>{
 
 elementoBtn.addEventListener("click", ()=>{
     if(elementoBtn.value == "primeiro"){
+        let som = new Audio("./assets/SONGS/audio02.mp3")
+        som.play();
         elementoBtn.innerText="Next"
         frase.innerText = `"Algumas das melhores histórias de amor começam com um homicídio."`;
         elementoImg.src = "./assets/IMG/imagem02.png";
         elementoImg.style.cssText = 'width: 46%;'+'margin-top: 3rem;';
         elementoBtn.value = "segundo";    
     }else if(elementoBtn.value == "segundo"){
+        let som = new Audio("./assets/SONGS/audio01.mp3")
+        som.play();
         frase.innerText = `"As ameaças doem, mas não tanto quanto uma faca serrilhada"`;
         elementoImg.src = "./assets/IMG/imagem03.png";
         elementoImg.style.cssText = 'width: 46%;'+'margin-top: 10rem;'+'margin-bottom:4rem;';
         elementoBtn.value = "terceiro";
     }else if(elementoBtn.value == "terceiro"){
+        let som = new Audio("./assets/SONGS/audio02.mp3")
+        som.play();
         frase.innerText = `"Água oxigenada e limão para remover manchas de sangue."`;
         elementoImg.src = "./assets/IMG/imagem04.png";
         elementoImg.style.cssText = 'width: 50%;'+'margin-top: 6rem;';
         elementoBtn.value = "quarto";
     }
     else if(elementoBtn.value == "quarto"){
+        let som = new Audio("./assets/SONGS/audio03.mp3")
+        som.play();
         frase.innerText = `"Contras de ser um super-herói: todos são idiotas treinados para respeitar a lei."`;
         elementoImg.src = "./assets/IMG/imagem06.png";
         elementoImg.style.cssText = 'width: 46%;';
         elementoBtn.value = "";
     }else{
+        let som = new Audio("./assets/SONGS/audio04.mp3")
+        som.play();
         elementoBtn.innerText="Recomeçar"
         frase.innerText = `Por hoje é só... Mas clica aí para ver de novo, vai!`;
         elementoImg.src = "./assets/IMG/imagem05.png";
@@ -58,3 +68,4 @@ elementoBtn.addEventListener("click", ()=>{
         elementoBtn.value = "primeiro";
     }
 });
+
